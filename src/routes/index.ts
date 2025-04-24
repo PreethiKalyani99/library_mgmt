66855fb02071317343cf5519bf81913615cc5614
PreethiKalyani99 <preethikalyani99@gmail.com>
2025-04-24 10:40:07 +0530

    register borrowed book, users and authors routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,19 @@
-import { Router } from 'express';
-import bookRoutes from './bookRoutes';
+import { Router } from 'express'
+import bookRoutes from './bookRoutes'
+import authorRoutes from './authorRoutes'
+import borrowedBookRoutes from './borrowedBookRoutes'
+import userRoutes from './userRoutes'
 
-const router = Router();
+const router = Router()
 
 // Register all routes
-router.use('/books', bookRoutes);
+router.use('/books', bookRoutes)
+router.use('/authors', authorRoutes)
+router.use('/borrowed-books', borrowedBookRoutes)
+router.use('/users', userRoutes)
 
 router.get('/health', (req, res) => {
-  res.send({ message: 'Hello World!!' });
-});
+  res.send({ message: 'Hello World!!' })
+})
 
-export default router; 
+export default router 
