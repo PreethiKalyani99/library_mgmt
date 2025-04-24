8e81eb80133898583d8a1ab9bcaff06a08e4df9c
PreethiKalyani99 <preethikalyani99@gmail.com>
2025-04-24 10:19:30 +0530

    creates user repository

---
+++ src/repositories/UserRepository.ts
@@ -0,0 +1,33 @@
+import { Repository } from "typeorm";
+import { User } from "../entities/User";
+import { AppDataSource } from "../config/database";
+
+export class UserRepository {
+    private repository: Repository<User>
+
+    constructor() {
+        this.repository = AppDataSource.getRepository(User)
+    }
+
+    async findAll(): Promise<User[]> {
+        return this.repository.find()
+    }
+
+    async findById(id: string): Promise<User | null> {
+        return this.repository.findOneBy({ id })
+    }
+
+    async findByEmail(email: string): Promise<User | null> {
+        return this.repository.findOneBy({ email })
+    }
+
+    async create(userData: Partial<User>): Promise<User> {
+        const user = this.repository.create(userData)
+        return this.repository.save(user)
+    }
+
+    async update(id: string, userData: Partial<User>): Promise<User | null> {
+        await this.repository.update(id, userData)
+        return this.findById(id)
+    }
+}
