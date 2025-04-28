54bd6d955fd3e775070be72662c1c77c64b1ee5a
PreethiKalyani99 <preethikalyani99@gmail.com>
2025-04-29 00:19:28 +0530

    feat: add function to update user password. refactor: move validations to route level using Joi. Make userRepository configurable via constructor for testing

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -2,16 +2,11 @@ import { hash } from "bcrypt"
 import { User } from "../entities/User"
 import { UserRepository } from "../repositories/UserRepository"
 
-interface UserCreationValidation {
-    email: string
-    password: string
-}
-
 export class UserService {
     private userRepository: UserRepository
 
-    constructor() {
-        this.userRepository = new UserRepository()
+    constructor(userRepository?: UserRepository) {
+        this.userRepository = userRepository ?? new UserRepository()
     }
 
     async getAllUsers(): Promise<User[]> {
@@ -22,32 +17,24 @@ export class UserService {
         return this.userRepository.findById(id)
     }
 
-    async validateUserCreation({ email, password }: Partial<UserCreationValidation>): Promise<User | null> {
-        if (!email) {
-            throw new Error('User email is required')
-        }
-        if (!password) {
-            throw new Error('User password is required')
-        }
-        if (password.length < 8) {
-            throw new Error('Password must be at least 8 characters long')
-        }
-
-        const user = await this.userRepository.findByEmail(email)
-        return user
-    }
-
-    async createUser(userData: Partial<User>): Promise<User> {
-        const user = await this.validateUserCreation({ email: userData.email, password: userData.password})
+    async createUser(userData: User): Promise<User> {
+        const user = await this.userRepository.findByEmail(userData.email)
 
         if (user) {
             throw new Error('User with this email already exists')
         }
-        if(!userData.password){
-            throw new Error('User password is required')
-        }
 
         userData.password = await hash(userData.password, 10)
         return this.userRepository.create(userData)
+    }
+
+    async updateUser(id: string, password: string): Promise<User | null> {
+        const user = await this.userRepository.findById(id)
+        if (!user) {
+            throw new Error('User not found')
+        }
+
+        user.password = await hash(password, 10)
+        return this.userRepository.update(id, user)
     }
 }
