84e60e62e1b1a0e2946ee4918c70d1e64af7d910
PreethiKalyani99 <preethikalyani99@gmail.com>
2025-04-29 00:14:36 +0530

    refactor-books: use QueryBuilder instead of repository findByOne method

--- src/repositories/BookRepository.ts
+++ src/repositories/BookRepository.ts
@@ -18,9 +18,12 @@ export class BookRepository {
   }
 
   async findByTitle(title: string): Promise<Book | null> {
-    return this.repository.findOneBy({ title })
+    return this.repository
+      .createQueryBuilder('book')
+      .where('LOWER(book.title) = LOWER(:title)', { title })
+      .getOne()
   }
-
+  
   async create(bookData: Partial<Book>): Promise<Book> {
     const book = this.repository.create(bookData)
     return this.repository.save(book)
