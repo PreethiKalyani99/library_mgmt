2ff0133cfba2c1818ae11c0b5f7fd2b84280a2d5
PreethiKalyani99 <preethikalyani99@gmail.com>
2025-04-29 00:24:09 +0530

    refactor: move validations to route level using Joi. Ensure book and user exist before updating. Make repository configurable via constructor for testing

--- src/services/BookService.ts
+++ src/services/BookService.ts
@@ -1,11 +1,20 @@
 import { Book } from '../entities/Book'
+import { Author } from '../entities/Author'
 import { BookRepository } from '../repositories/BookRepository'
+import { AuthorRepository } from '../repositories/AuthorRepository'
+
+interface AuthorDataType {
+  id: string
+  name: string
+}
 
 export class BookService {
   private bookRepository: BookRepository
+  private authorRepository: AuthorRepository
 
-  constructor(bookRepository?: BookRepository) {
+  constructor(bookRepository?: BookRepository, authorRepository?: AuthorRepository) {
     this.bookRepository = bookRepository ?? new BookRepository()
+    this.authorRepository = authorRepository ?? new AuthorRepository()
   }
 
   async getAllBooks(): Promise<Book[]> {
@@ -16,19 +25,42 @@ export class BookService {
     return this.bookRepository.findById(id)
   }
 
-  async createBook(bookData: Partial<Book>): Promise<Book> {
-    // Add any business logic/validation here
-    if (!bookData.title || (!bookData?.author?.name && !bookData?.author?.id)) {
-      throw new Error('Title and author are required')
+  async findAuthorBy({ id, name }: AuthorDataType): Promise<Author | null> {
+    if (id) {
+      return this.authorRepository.findById(id)
     }
+    return this.authorRepository.findByName(name)
+  }
+  
+  async createBook(bookData: Book): Promise<Book> {
+    // Add any business logic/validation here
+    const book = await this.bookRepository.findByTitle(bookData.title)
+    if (book) {
+      throw new Error('Book with this title already exists')
+    }
+    
+    const author = await this.findAuthorBy({id: bookData?.author?.id, name: bookData?.author?.name})
+    if(!author) {
+      throw new Error('Author does not exist')
+    }
+    bookData.author = author
     return this.bookRepository.create(bookData)
   }
 
   async updateBook(id: string, bookData: Partial<Book>): Promise<Book | null> {
-    const existingBook = await this.bookRepository.findById(id)
-    if (!existingBook) {
+    const book = await this.bookRepository.findById(id)
+    if (!book) {
       throw new Error('Book not found')
     }
+
+    if(bookData?.author?.name || bookData?.author?.id) {
+      const author = await this.findAuthorBy({id: bookData?.author?.id, name: bookData?.author?.name})
+      if(!author) {
+        throw new Error('Author does not exist')
+      }
+      bookData.author = author
+    }
+
     return this.bookRepository.update(id, bookData)
   }
 
